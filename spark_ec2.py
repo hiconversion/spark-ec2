5ef194bb0bc5b77857cb8cb2bfb87b1acbcd3323
Kavika <dhenrickson@hiconversion.com>
2016-11-23 13:43:16 -0800

    spark_ec2.py x1 instance types

--- spark_ec2.py
+++ spark_ec2.py
@@ -446,6 +446,8 @@ EC2_INSTANCE_TYPES = {
     "t2.small":    "hvm",
     "t2.medium":   "hvm",
     "t2.large":    "hvm",
+    "x1.16xlarge": "hvm",
+    "x1.32xlarge": "hvm",
 }
 
 
@@ -1022,6 +1024,8 @@ def get_num_disks(instance_type):
         "t2.small":    0,
         "t2.medium":   0,
         "t2.large":    0,
+        "x1.16xlarge": 1,
+        "x1.32xlarge": 2,
     }
     if instance_type in disks_by_instance:
         return disks_by_instance[instance_type]
