3ed4d1f69111c4f60ca249438d27a853ad6cdd5a
Jeff McKelvey <jeff.mckelvey@gmail.com>
2021-01-18 14:51:16 -0500

    branch-2.1 started

--- spark_ec2.py
+++ spark_ec2.py
@@ -128,7 +128,7 @@ DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
 DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/amplab/spark-ec2"
-DEFAULT_SPARK_EC2_BRANCH = "branch-2.0"
+DEFAULT_SPARK_EC2_BRANCH = "branch-2.1"
 
 
 def setup_external_libs(libs):
